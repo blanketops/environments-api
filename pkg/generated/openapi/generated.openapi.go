9a09c3efe1190bb72b4238ee6909b014a7d42011
Neo <ntlaletsi86@gmail.com>
2026-02-21 22:55:12 +0200

    ApiServer #, Include all missing types

--- pkg/generated/openapi/generated.openapi.go
+++ pkg/generated/openapi/generated.openapi.go
@@ -14,21 +14,41 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/ntlaletsi70/blanketops-environments-api/api/environments/v1alpha1.EnvironmentList":   schemaEnvironmentList(ref),
 		"github.com/ntlaletsi70/blanketops-environments-api/api/environments/v1alpha1.EnvironmentSpec":   schemaEnvironmentSpec(ref),
 		"github.com/ntlaletsi70/blanketops-environments-api/api/environments/v1alpha1.EnvironmentStatus": schemaEnvironmentStatus(ref),
-		// apimachinery types required by the apiserver internals
-		"io.k8s.apimachinery.pkg.version.Info":                 schemaVersionInfo(ref),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.APIGroupList":    schemaSimpleObject("APIGroupList"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.APIGroup":        schemaSimpleObject("APIGroup"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.APIVersions":     schemaSimpleObject("APIVersions"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.APIResourceList": schemaSimpleObject("APIResourceList"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.Status":          schemaSimpleObject("Status"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.WatchEvent":      schemaSimpleObject("WatchEvent"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta":      schemaSimpleObject("ObjectMeta"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.ListMeta":        schemaSimpleObject("ListMeta"),
-		"io.k8s.apimachinery.pkg.apis.meta.v1.Condition":       schemaSimpleObject("Condition"),
-		"io.k8s.apimachinery.pkg.runtime.RawExtension":         schemaSimpleObject("RawExtension"),
+		// apimachinery meta v1
+		"io.k8s.apimachinery.pkg.apis.meta.v1.APIGroupList":              schemaSimpleObject("APIGroupList"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.APIGroup":                  schemaSimpleObject("APIGroup"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.APIVersions":               schemaSimpleObject("APIVersions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.APIResourceList":           schemaSimpleObject("APIResourceList"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.APIResource":               schemaSimpleObject("APIResource"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.Status":                    schemaSimpleObject("Status"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails":             schemaSimpleObject("StatusDetails"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause":               schemaSimpleObject("StatusCause"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.WatchEvent":                schemaSimpleObject("WatchEvent"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta":                schemaSimpleObject("ObjectMeta"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.ListMeta":                  schemaSimpleObject("ListMeta"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.Condition":                 schemaSimpleObject("Condition"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.DeleteOptions":             schemaSimpleObject("DeleteOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.CreateOptions":             schemaSimpleObject("CreateOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.UpdateOptions":             schemaSimpleObject("UpdateOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.PatchOptions":              schemaSimpleObject("PatchOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.GetOptions":                schemaSimpleObject("GetOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.ListOptions":               schemaSimpleObject("ListOptions"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.OwnerReference":            schemaSimpleObject("OwnerReference"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.LabelSelector":             schemaSimpleObject("LabelSelector"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.LabelSelectorRequirement":  schemaSimpleObject("LabelSelectorRequirement"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.ManagedFieldsEntry":        schemaSimpleObject("ManagedFieldsEntry"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.FieldsV1":                  schemaSimpleObject("FieldsV1"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.Time":                      schemaSimpleObject("Time"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.Duration":                  schemaSimpleObject("Duration"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.Patch":                     schemaSimpleObject("Patch"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.RootPaths":                 schemaSimpleObject("RootPaths"),
+		"io.k8s.apimachinery.pkg.apis.meta.v1.ServerAddressByClientCIDR": schemaSimpleObject("ServerAddressByClientCIDR"),
+		// apimachinery runtime
+		"io.k8s.apimachinery.pkg.runtime.RawExtension": schemaSimpleObject("RawExtension"),
+		// apimachinery version
+		"io.k8s.apimachinery.pkg.version.Info": schemaVersionInfo(ref),
 	}
 }
-
 func schemaSimpleObject(description string) common.OpenAPIDefinition {
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
