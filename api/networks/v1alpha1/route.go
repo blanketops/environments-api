1ad361743706edd76fea7a3d710cacc0d92a0c7b
Neo Tlaletsi <ntlaletsi86@gmail.com>
2026-07-01 22:29:53 +0200

    Update route.go

--- api/networks/v1alpha1/route.go
+++ api/networks/v1alpha1/route.go
@@ -12,9 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package v1alpha1
 
 // +k8s:openapi-gen=true
+package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
