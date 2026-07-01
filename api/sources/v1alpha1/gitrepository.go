4b9f64f7d2faaea342da6edc6fdf55af90b66320
Neo Tlaletsi <ntlaletsi86@gmail.com>
2026-07-01 22:25:59 +0200

    Update gitrepository.go

--- api/sources/v1alpha1/gitrepository.go
+++ api/sources/v1alpha1/gitrepository.go
@@ -13,16 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-*/
-
 // +k8s:openapi-gen=true
 
 package v1alpha1
